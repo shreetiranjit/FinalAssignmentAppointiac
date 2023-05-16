932c5c004afcb0b30fe2a252f3615ede15af1908
shreetiranjit <ranjit.shreeti1@gmail.com>
2023-05-16 18:58:29 +0545

    made folder structure amd added necessary dependencies .

---
+++ lib/myapp.dart
@@ -0,0 +1,24 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      title: 'Appointiac',
+      home: const Scaffold(
+        // body: LoadingAnimation(),
+        // drawer: NavBar(),
+      ),
+    );
+  }
+}
