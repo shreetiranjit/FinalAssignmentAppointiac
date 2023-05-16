fafb2f16d309edc4151b8012a7976eac17568def
shreetiranjit <ranjit.shreeti1@gmail.com>
2023-05-16 19:01:56 +0545

    added simple splash screen.

---
+++ lib/common/splashscreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoadingAnimation extends StatefulWidget {
+  const LoadingAnimation({super.key});
+
+  @override
+  LoadingAnimationState createState() => LoadingAnimationState();
+}
+
+class LoadingAnimationState extends State<LoadingAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    )..repeat();
+    Future.delayed(const Duration(seconds: 2), () {
+      // navigate to the main screen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WelcomePage()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RotationTransition(
+        turns: Tween(begin: 0.0, end: 1.0).animate(_controller!),
+        child: Image.asset(
+          'assets/loading_image.png',
+          width: 100,
+          height: 200,
+          color: const Color(0xFFFF6C3F),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/welcomepage/welcomepage_one.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomePageOne extends StatelessWidget {
+  const WelcomePageOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
